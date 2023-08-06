d96c8609c9e78e3d7046cb32d97a607d7ffd84e8
Hiroto1403 <113161371+Hiroto1403@users.noreply.github.com>
2023-08-06 16:18:17 +0900

    spawn
    todya,work

--- Assets/sakana(hiroto)/Script/Enemy_Spawn.cs
+++ Assets/sakana(hiroto)/Script/Enemy_Spawn.cs
@@ -6,11 +6,11 @@ public class Enemy_Spawn : MonoBehaviour
 {
     //ゲームオブジェクトを取得
     public GameObject EnemyPrefab;
-    public GameObject fireingPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Spawnを一秒間間隔で実行する
+        InvokeRepeating("Spawn", 3f, 3f);
     }
 
     // Update is called once per frame
@@ -18,12 +18,18 @@ public class Enemy_Spawn : MonoBehaviour
     {
         
     }
+    
     //Enemyを生成
+
     private void Spawn()
     {
+        //ランダムなY軸を取得
+        Vector2 randomPos= new Vector2(
+           Random.Range(-1.0f,1.0f),
+           transform.position.x);
         Instantiate(
             EnemyPrefab,//生成したいオブジェクト
-            fireingPosition.transform.position,//位置
+            randomPos,//位置
             transform.rotation);//回転
     }
 }
