982abcfe1e72a85189fed40c631de3767e5724d6
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2023-08-07 14:44:00 +0900

    使ってない変数を削除、変数名変更
    親機：プレイヤー
    子機：エレメント
    で統一

--- Assets/Yoshino/Scripts/PlayerController.cs
+++ Assets/Yoshino/Scripts/PlayerController.cs
@@ -16,43 +16,35 @@ public class PlayerController : MonoBehaviour
     private GameObject m_head = null;
     //プレイヤーと、子機のメンバ変数
     private GameObject m_player = null;
-    private GameObject m_sub1 = null;
-    private GameObject m_sub2 = null;
+    private GameObject m_element1 = null;
+    private GameObject m_element2 = null;
 
     private Rigidbody2D m_playerRb = null;
-
-    private Vector3 m_playerPos = Vector3.zero;
-
-    private Transform m_subTf1 = null;
-    private Transform m_subTf2 = null;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        //メンバ変数に代入
         m_player = m_pigs[0];
-        m_sub1 = m_pigs[1];
-        m_sub2 = m_pigs[2];
+        m_element1 = m_pigs[1];
+        m_element2 = m_pigs[2];
         m_playerRb = m_player.GetComponent<Rigidbody2D>();
-        m_subTf1 = m_sub1.transform;
-        m_subTf2 = m_sub2.transform;
         //プレイヤーと子機のタグを変更
         m_player.tag = "Player";
-        m_sub1.tag = m_sub2.tag = "Untagged";
+        m_element1.tag = m_element2.tag = "Untagged";
         //プレイヤーのlayerをPlayerにそれ以外をその後ろに変更
         m_player.layer = 7;
         m_pigs[1].layer = 8;
         m_pigs[2].layer = 9;
         //Playerを最前面に、子機をその後ろに映るようにする
         m_player.GetComponentInChildren<SpriteRenderer>().sortingOrder = 3;
-        m_sub1.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
-        m_sub2.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
+        m_element1.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
+        m_element2.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_playerPos = m_player.transform.position;
         m_playerRb.velocity = Vector2.zero;
         //プレイヤー(操作するキャラクター)を変更
         if (Input.GetKeyDown(KeyCode.Space))
@@ -61,46 +53,8 @@ public class PlayerController : MonoBehaviour
         }
         PlayerMove();
 
-
-        /*
-        //プレイヤーに追従させる処理
-        float dist1 = Vector3.Distance(m_subTf1.position, m_playerPos);
-        if (dist1 > m_distanceMin)
-        {
-            //ターゲットの方向を求める
-            Vector3 dir = (m_playerPos - m_subTf1.position).normalized;
-            //ターゲットの方向に移動する
-            m_subTf1.position += dir * m_speed * Time.deltaTime;
-            //追従するオブジェクトへ向ける
-            if (dir.x >= 0)
-            {
-                m_subTf1.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                m_subTf1.localScale = new Vector3(-1, 1, 1);
-            }
-        }
-        float dist2 = Vector3.Distance(m_subTf2.position, m_subTf1.position);
-        if (dist2 > m_distanceMin)
-        {
-            //ターゲットの方向を求める
-            Vector3 dir = (m_subTf1.position - m_subTf2.position).normalized;
-            //ターゲットの方向に移動する
-            m_subTf2.position += dir * m_speed * Time.deltaTime;
-            //追従するオブジェクトへ向ける
-            if (dir.x >= 0)
-            {
-                m_subTf2.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                m_subTf2.localScale = new Vector3(-1, 1, 1);
-            }
-        }*/
-
-        FollowUp(m_sub1, m_player);
-        FollowUp(m_sub2, m_sub1);
+        FollowUp(m_element1, m_player);
+        FollowUp(m_element2, m_element1);
     }
         
     //プレイヤーの移動処理
@@ -133,63 +87,43 @@ public class PlayerController : MonoBehaviour
         m_pigs.Add(m_head);
         //メンバ変数に代入
         m_player = m_pigs[0];
-        m_sub1 = m_pigs[1];
-        m_sub2 = m_pigs[2];
+        m_element1 = m_pigs[1];
+        m_element2 = m_pigs[2];
         m_playerRb = m_player.GetComponent<Rigidbody2D>();
-        m_subTf1 = m_pigs[1].transform;
-        m_subTf2 = m_pigs[2].transform;
         //プレイヤーと子機のタグを変更
         m_player.tag = "Player";
-        m_sub1.tag = m_sub2.tag = "Untagged";
+        m_element1.tag = m_element2.tag = "Untagged";
         //プレイヤーのlayerをPlayerにそれ以外をその後ろに変更
         m_player.layer = 7;
         m_pigs[1].layer = 8;
         m_pigs[2].layer = 9;
         //Playerを最前面に、子機をその後ろに映るようにする
         m_player.GetComponentInChildren<SpriteRenderer>().sortingOrder = 3;
-        m_sub1.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
-        m_sub2.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
+        m_element1.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
+        m_element2.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
     }
 
-    //追従するオブジェクト、追従のターゲット
-    private void FollowUp(GameObject sub, GameObject parent)
+    //追従するオブジェクト、追従するターゲット
+    private void FollowUp(GameObject element, GameObject target)
     {
-        Transform parentTf = parent.transform;
-        Transform subTf = sub.transform;
+        Transform parentTf = target.transform;
+        Transform subTf = element.transform;
         float dist = Vector3.Distance(subTf.position, parentTf.position);
         if (dist > m_distanceMin)
         {
             // ターゲットの方向を求める
             Vector3 dir = (parentTf.position - subTf.position).normalized;
             //ターゲットの方向に移動する
-            sub.transform.position += dir * m_speed * Time.deltaTime;
+            element.transform.position += dir * m_speed * Time.deltaTime;
             //追従するオブジェクトへ向ける
             if (dir.x >= 0)
             {
-                sub.transform.localScale = new Vector3(1, 1, 1);
+                element.transform.localScale = new Vector3(1, 1, 1);
             }
             else
             {
-                sub.transform.localScale = new Vector3(-1, 1, 1);
+                element.transform.localScale = new Vector3(-1, 1, 1);
             }
         }
     }
-//    //プレイヤーに追従させる処理
-//    float dist1 = Vector3.Distance(m_subTf1.position, m_playerPos);
-//        if (dist1 > m_distanceMin)
-//        {
-//            //ターゲットの方向を求める
-//            Vector3 dir = (m_playerPos - m_subTf1.position).normalized;
-//    //ターゲットの方向に移動する
-//    m_subTf1.position += dir* m_speed * Time.deltaTime;
-//            //追従するオブジェクトへ向ける
-//            if (dir.x >= 0)
-//            {
-//                m_subTf1.localScale = new Vector3(1, 1, 1);
-//}
-//            else
-//{
-//    m_subTf1.localScale = new Vector3(-1, 1, 1);
-//}
-//        }
 }
